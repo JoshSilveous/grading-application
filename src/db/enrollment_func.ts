b442970cd1a81c0bd4920593519b5d8247468984
JoshSilveous <JoshSilveous@gmail.com>
2023-03-31 22:30:01 -0400

    added exports to enrollment_func

--- src/db/enrollment_func.ts
+++ src/db/enrollment_func.ts
@@ -16,3 +16,25 @@ function deleteEnrollment(class_id: Number, student_id: Number): void {
     `
     db.exec(sql)
 }
+
+declare global {
+    interface enrollment_func_exports {
+        /**
+         * Adds an enrollment to the database.
+         * @param class_id The ID of the class.
+         * @param student_id The ID of the student.
+         */
+        addEnrollment: (class_id: Number, student_id: Number) => void,
+        /**
+         * Deletes an enrollment from the database.
+         * @param class_id The ID of the class.
+         * @param student_id The ID of the student.
+         */
+        deleteEnrollment: (class_id: Number, student_id: Number) => void
+    }
+}
+
+module.exports = {
+    addEnrollment,
+    deleteEnrollment
+} as enrollment_func_exports
