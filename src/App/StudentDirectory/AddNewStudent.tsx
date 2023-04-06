2357e5bf9611782818c97e08adcfd6d8a7149c5d
Joshua Silveous <JoshSilveous@gmail.com>
2023-04-06 16:41:33 -0400

    added external file to store "New Student" modal

---
+++ src/App/StudentDirectory/AddNewStudent.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const newStudentPopupContent = <div>Hello! new student module here</div>
+
+
+interface AddNewStudent_exports {
+    newStudentPopupContent: JSX.Element
+}
+export default {
+    newStudentPopupContent
+} as AddNewStudent_exports
