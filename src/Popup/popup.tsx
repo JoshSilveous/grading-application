ce532d4d495c8080b46889c7f547ee52dd0fc9e9
JoshSilveous <JoshSilveous@gmail.com>
2023-04-11 21:34:12 -0400

    added param to provide function to run after close

--- src/Popup/popup.tsx
+++ src/Popup/popup.tsx
@@ -4,16 +4,23 @@ import './popup.scss'
 
 const popupDomLocation = ReactDOM.createRoot(document.getElementById('popup-root'))
 
-
 function closePopup() {
     popupDomLocation.render(<></>)
 }
 
-function triggerPopup(content: JSX.Element, specialType?: "warning") {
+
+function triggerPopup(content: JSX.Element, specialType?: "warning", handleClose?: () => void) {
+
+
     const popupFinal =
-        <div className="popup-background" >
+    <div className="popup-background" >
             <div className={`popup-container ${specialType === "warning" ? "warning" : ""}`}>
-                <div className="popup-exit" onClick={closePopup}>✖</div>
+                <div className="popup-exit" 
+                    onClick={() => {
+                        closePopup()
+                        if (handleClose) {handleClose}
+                    }}
+                >✖</div>
                 {content}
             </div>
         </div>
@@ -26,8 +33,13 @@ interface popup_exports {
      * Creates a blocking popup on the screen. Overrides other popups.
      * @param content The content to hold within the popup.
      * @param specialType `"warning"` will make the popup tinted red.
+     * @param handleClose A callback function that is ran when the popup is closed by the user pressing the `x` button.
      */
-    triggerPopup: (content: JSX.Element, specialType?: "warning") => void,
+    triggerPopup: (
+        content: JSX.Element, 
+        specialType?: "warning",
+        handleClose?: () => void
+    ) => void,
     /**
      * Closes the current popup.
      */
